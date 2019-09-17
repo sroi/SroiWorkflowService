0e6a4857f9f88033d0762c4d204c127e76e2c2dd
sroi <bachkarakash@gmail.com>
2019-09-17 13:40:10 +0000

    cross origin added
    cross origin added

--- src/main/java/benefitBountyService/controllers/ProjectController.java
+++ src/main/java/benefitBountyService/controllers/ProjectController.java
@@ -8,18 +8,17 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/project")
 public class ProjectController {
+
 
     @Autowired
     private ProjectService projectService;
